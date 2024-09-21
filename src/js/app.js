8fb6fac385e805af792a267df8f45bb5ce41a8d9
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2024-09-21 22:35:55 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,20 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper('.partners__swiper', {
+    loop: true,
+    slidesPerView: 2,
+    modules: [Autoplay],
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        576: {
+            slidesPerView: 4,
+        },
+    }
+});
